cc47ed5d29388d7a043a5197dc626e5be3482ba0
SanyaSriv <ssrivas7@ucsc.edu>
2023-05-08 01:08:49 -0700

    Changing some variable names

--- main3.py
+++ main3.py
@@ -18,10 +18,10 @@ box_pos = (padding, padding)
 router_radius = 40
 
 # POsition of Area 1 routers
-box_1_pos = (200, 250)
-box_2_pos = (355, 380)
-box_3_pos = (350, 515)
-box_4_pos = (70, 380)
+router_1_pos = (200, 250)
+router_2_pos = (355, 380)
+router_3_pos = (350, 515)
+router_4_pos = (70, 380)
 
 # Position of Area 1 Swicthes
 switch_1_pos = (170, 350)
@@ -48,14 +48,14 @@ pygame.draw.rect(screen, (240, 240, 230), (area_3_pos[0], area_3_pos[1], box_siz
 pygame.draw.rect(screen, (240, 230, 238), (area_4_pos[0], area_4_pos[1], box_size * 3.5, box_size * 3))
 
 # Mkaing routers for area 1: All routers will be circles
-pygame.draw.circle(screen, (159, 173, 191), (box_1_pos[0], box_1_pos[1]), router_radius)
-pygame.draw.circle(screen, (159, 173, 191), (box_2_pos[0], box_2_pos[1]), router_radius)
-pygame.draw.circle(screen, (159, 173, 191), (box_3_pos[0], box_3_pos[1]), router_radius)
-pygame.draw.circle(screen, (159, 173, 191), (box_4_pos[0], box_4_pos[1]), router_radius)
+pygame.draw.circle(screen, (159, 173, 191), (router_1_pos[0], router_1_pos[1]), router_radius)
+pygame.draw.circle(screen, (159, 173, 191), (router_2_pos[0], router_2_pos[1]), router_radius)
+pygame.draw.circle(screen, (159, 173, 191), (router_3_pos[0], router_3_pos[1]), router_radius)
+pygame.draw.circle(screen, (159, 173, 191), (router_4_pos[0], router_4_pos[1]), router_radius)
 
 # Making the switches for area 1: Rectangles
-pygame.draw.rect(screen, (163, 122, 113), (switch_1_pos[0], switch_1_pos[1], switch_size, switch_size))
-pygame.draw.rect(screen, (163, 122, 113), (switch_2_pos[0], switch_2_pos[1], switch_size, switch_size))
+pygame.draw.rect(screen, (222, 146, 146), (switch_1_pos[0], switch_1_pos[1], switch_size, switch_size))
+pygame.draw.rect(screen, (222, 146, 146), (switch_2_pos[0], switch_2_pos[1], switch_size, switch_size))
 
 # Render text to display inside each box
 font = pygame.font.SysFont('Arial', 14)
@@ -70,10 +70,10 @@ text_6 = font.render('Switch 2', True, (0, 0, 0))
 text_pos = ((box_size - font.size('Router 1')[0]) // 2, (box_size - font.size('Router 1')[1]) // 2)
 text_pos_switch = ((switch_size - font.size('Switch 1')[0]) // 2, (switch_size - font.size('Switch 1')[1]) // 2)
 
-screen.blit(text_1, text_1.get_rect(center=box_1_pos))
-screen.blit(text_2, text_1.get_rect(center=box_2_pos))
-screen.blit(text_3, text_1.get_rect(center=box_3_pos))
-screen.blit(text_4, text_1.get_rect(center=box_4_pos))
+screen.blit(text_1, text_1.get_rect(center=router_1_pos))
+screen.blit(text_2, text_1.get_rect(center=router_2_pos))
+screen.blit(text_3, text_1.get_rect(center=router_3_pos))
+screen.blit(text_4, text_1.get_rect(center=router_4_pos))
 
 # Display the text inside each box
 screen.blit(text_5, (switch_1_pos[0] + text_pos_switch[0], switch_1_pos[1] + text_pos_switch[1]))
@@ -84,43 +84,43 @@ screen.blit(text_6, (switch_2_pos[0] + text_pos_switch[0], switch_2_pos[1] + tex
 # switch 1 --> router 1
 center_box = (switch_1_pos[0] + switch_size // 2, switch_1_pos[1])
 # finding the closest point
-x = center_box[0] - box_1_pos[0]
-y = center_box[1] - box_1_pos[1]
+x = center_box[0] - router_1_pos[0]
+y = center_box[1] - router_1_pos[1]
 a = math.atan2(y, x)
-closest_point = (int(box_1_pos[0] + router_radius * math.cos(a)), int(box_1_pos[1] + router_radius * math.sin(a)))
+closest_point = (int(router_1_pos[0] + router_radius * math.cos(a)), int(router_1_pos[1] + router_radius * math.sin(a)))
 pygame.draw.line(screen, (0, 0, 0),  closest_point, center_box, 3)
 
 # switch 1 --> router 4
 center_box = (switch_1_pos[0], switch_1_pos[1] + switch_size // 2)
-x = center_box[0] - box_4_pos[0]
-y = center_box[1] - box_4_pos[1]
+x = center_box[0] - router_4_pos[0]
+y = center_box[1] - router_4_pos[1]
 a = math.atan2(y, x)
-closest_point = (int(box_4_pos[0] + router_radius * math.cos(a)), int(box_4_pos[1] + router_radius * math.sin(a)))
+closest_point = (int(router_4_pos[0] + router_radius * math.cos(a)), int(router_4_pos[1] + router_radius * math.sin(a)))
 pygame.draw.line(screen, (0, 0, 0),  closest_point, center_box, 3)
 
 # router 4 --> switch 2
 center_box = (switch_2_pos[0] + switch_size // 2, switch_2_pos[1])
-x = center_box[0] - box_4_pos[0]
-y = center_box[1] - box_4_pos[1]
+x = center_box[0] - router_4_pos[0]
+y = center_box[1] - router_4_pos[1]
 a = math.atan2(y, x)
-closest_point = (int(box_4_pos[0] + router_radius * math.cos(a)), int(box_4_pos[1] + router_radius * math.sin(a)))
+closest_point = (int(router_4_pos[0] + router_radius * math.cos(a)), int(router_4_pos[1] + router_radius * math.sin(a)))
 pygame.draw.line(screen, (0, 0, 0),  closest_point, center_box, 3)
 
 
 # switch 2 --> router 3
 center_box = (switch_2_pos[0] + switch_size, switch_2_pos[1] + switch_size // 2)
-x = center_box[0] - box_3_pos[0]
-y = center_box[1] - box_3_pos[1]
+x = center_box[0] - router_3_pos[0]
+y = center_box[1] - router_3_pos[1]
 a = math.atan2(y, x)
-closest_point = (int(box_3_pos[0] + router_radius * math.cos(a)), int(box_3_pos[1] + router_radius * math.sin(a)))
+closest_point = (int(router_3_pos[0] + router_radius * math.cos(a)), int(router_3_pos[1] + router_radius * math.sin(a)))
 pygame.draw.line(screen, (0, 0, 0),  closest_point, center_box, 3)
 
 # switch 1 --> router 2
 center_box = (switch_1_pos[0] + switch_size, switch_1_pos[1] + switch_size // 2)
-x = center_box[0] - box_2_pos[0]
-y = center_box[1] - box_2_pos[1]
+x = center_box[0] - router_2_pos[0]
+y = center_box[1] - router_2_pos[1]
 a = math.atan2(y, x)
-closest_point = (int(box_2_pos[0] + router_radius * math.cos(a)), int(box_2_pos[1] + router_radius * math.sin(a)))
+closest_point = (int(router_2_pos[0] + router_radius * math.cos(a)), int(router_2_pos[1] + router_radius * math.sin(a)))
 pygame.draw.line(screen, (0, 0, 0),  closest_point, center_box, 3)
 
 # Update the screen

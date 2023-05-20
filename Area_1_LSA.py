03e2f36903b27b2374ffa4a3d8a33431ce7b5060
SanyaSriv <ssrivas7@ucsc.edu>
2023-05-20 16:21:46 -0700

    LSA1 of 3 routers working

--- Area_1_LSA.py
+++ Area_1_LSA.py
@@ -6,39 +6,90 @@ from global_variables_file import *
 list_of_Area_1_triangles = []
 
 def initialize_Area1_LSA():
-    # I need 1 trianglesfrom router 1 (for now)
-    var = LSATriangles(area_1_travel_routes["router_5_area_1__TO__router_1"][0], 
-                                                 area_1_travel_routes["router_5_area_1__TO__router_1"][1], 
-                                                 (120, 255, 100))
-    var.enable_full_route([area_1_travel_routes["router_5_area_1__TO__router_1"][0],
-                                                  area_1_travel_routes["router_5_area_1__TO__router_1"][1],
-                                                  area_1_travel_routes["router_5_area_1__TO__router_2"][1],
-                                                  area_1_travel_routes["router_5_area_1__TO__router_2"][0]])
-    list_of_Area_1_triangles.append(var)
+    # ROUTER 1 LSA 1
+    router_1_t1 = LSATriangles(area_1_travel_routes["router_5_area_1__TO__router_1"][0], 
+                                            area_1_travel_routes["router_5_area_1__TO__router_1"][1], 
+                                            (137, 25, 181))
+    router_1_t1.enable_full_route([area_1_travel_routes["router_5_area_1__TO__router_1"][0],
+                                                area_1_travel_routes["router_5_area_1__TO__router_1"][1],
+                                                area_1_travel_routes["router_5_area_1__TO__router_2"][1],
+                                                area_1_travel_routes["router_5_area_1__TO__router_2"][0],
+                                                area_1_travel_routes["router_2__TO__router_6"][0],
+                                                area_1_travel_routes["router_2__TO__router_6"][1]])
+    
+    router_1_t2 = LSATriangles(area_1_travel_routes["router_5_area_1__TO__router_1"][0], 
+                                                area_1_travel_routes["router_5_area_1__TO__router_1"][1], 
+                                                (137, 25, 181))
+    router_1_t2.enable_full_route([area_1_travel_routes["router_5_area_1__TO__router_1"][0],
+                                                area_1_travel_routes["router_5_area_1__TO__router_1"][1],
+                                                area_1_travel_routes["router_5_area_1__TO__router_4"][1],
+                                                area_1_travel_routes["router_5_area_1__TO__router_4"][0],
+                                                area_1_travel_routes["router_4__TO__router_3"][1],
+                                                area_1_travel_routes["router_4__TO__router_3"][0]])
+    
+    list_of_Area_1_triangles.append(router_1_t1)
+    list_of_Area_1_triangles.append(router_1_t2)
+
+    # ROUTER 2 LSA 1
+    router_2_t1 = LSATriangles(area_1_travel_routes["router_2__TO__router_6"][0], 
+                                            area_1_travel_routes["router_2__TO__router_6"][1], 
+                                            (181, 121, 25))
+    router_2_t1.enable_full_route([area_1_travel_routes["router_2__TO__router_6"][0],
+                                                area_1_travel_routes["router_2__TO__router_6"][1]])
+    
+    router_2_t2 = LSATriangles(area_1_travel_routes["router_5_area_1__TO__router_2"][0], 
+                                            area_1_travel_routes["router_5_area_1__TO__router_2"][1], 
+                                            (181, 121, 25))
+    router_2_t2.enable_full_route([area_1_travel_routes["router_5_area_1__TO__router_2"][0],
+                                                area_1_travel_routes["router_5_area_1__TO__router_2"][1],
+                                                area_1_travel_routes["router_5_area_1__TO__router_1"][1],
+                                                area_1_travel_routes["router_5_area_1__TO__router_1"][0]])
+    
+    router_2_t3 = LSATriangles(area_1_travel_routes["router_5_area_1__TO__router_2"][0], 
+                                            area_1_travel_routes["router_5_area_1__TO__router_2"][1], 
+                                            (181, 121, 25))
+    router_2_t3.enable_full_route([area_1_travel_routes["router_5_area_1__TO__router_2"][0],
+                                                area_1_travel_routes["router_5_area_1__TO__router_2"][1],
+                                                area_1_travel_routes["router_5_area_1__TO__router_4"][1],
+                                                area_1_travel_routes["router_5_area_1__TO__router_4"][0],
+                                                area_1_travel_routes["router_4__TO__router_3"][1],
+                                                area_1_travel_routes["router_4__TO__router_3"][0]])
+    
+    list_of_Area_1_triangles.append(router_2_t1)
+    list_of_Area_1_triangles.append(router_2_t2)
+    list_of_Area_1_triangles.append(router_2_t3)
+
+
+    # ROUTER 5 LSA 1
+    router_5_t1 = LSATriangles(area_1_travel_routes["router_5_area_1__TO__router_1"][1], 
+                                            area_1_travel_routes["router_5_area_1__TO__router_1"][0], 
+                                            (50, 166, 168))
+    router_5_t1.enable_full_route([area_1_travel_routes["router_5_area_1__TO__router_1"][1],
+                                                area_1_travel_routes["router_5_area_1__TO__router_1"][0]])
+    
+    router_5_t2 = LSATriangles(area_1_travel_routes["router_5_area_1__TO__router_4"][1], 
+                                            area_1_travel_routes["router_5_area_1__TO__router_4"][0], 
+                                            (50, 166, 168))
+    router_5_t2.enable_full_route([area_1_travel_routes["router_5_area_1__TO__router_4"][1],
+                                                area_1_travel_routes["router_5_area_1__TO__router_4"][0],
+                                                area_1_travel_routes["router_4__TO__router_3"][1],
+                                                area_1_travel_routes["router_4__TO__router_3"][0]])
+    
+    router_5_t3 = LSATriangles(area_1_travel_routes["router_5_area_1__TO__router_2"][1], 
+                                            area_1_travel_routes["router_5_area_1__TO__router_2"][0], 
+                                            (50, 166, 168))
+    router_5_t3.enable_full_route([area_1_travel_routes["router_5_area_1__TO__router_2"][1],
+                                                area_1_travel_routes["router_5_area_1__TO__router_2"][0],
+                                                area_1_travel_routes["router_2__TO__router_6"][0],
+                                                area_1_travel_routes["router_2__TO__router_6"][1]])
+
+    list_of_Area_1_triangles.append(router_5_t1)
+    list_of_Area_1_triangles.append(router_5_t2)
+    list_of_Area_1_triangles.append(router_5_t3)
+
 
 def SendArea1LSA1(pygame, screen):
-    global line
     
     for i in list_of_Area_1_triangles:
         i.draw(pygame, screen)
         i.move_triangle()
-
-    # dx = global_variables_file.triangle_target[0] - global_variables_file.triangle_pos[0]
-    # dy = global_variables_file.triangle_target[1] - global_variables_file.triangle_pos[1]
-    # distance = math.sqrt(dx ** 2 + dy ** 2)
-    # direction = (dx / distance, dy / distance)
-    
-    # # Update triangle position
-    # if distance > triangle_speed:
-
-    #     global_variables_file.triangle_pos = (
-    #         global_variables_file.triangle_pos[0] + direction[0] * triangle_speed,
-    #         global_variables_file.triangle_pos[1] + direction[1] * triangle_speed
-    # )
-    # elif (line == 0):
-    #     # now it should travel from 5 to 2
-    #     global_variables_file.triangle_pos = area_1_travel_routes["router_5_area_1__TO__router_2"][1]
-    #     triangle_list.append([global_variables_file.triangle_pos])
-    #     global_variables_file.triangle_target = area_1_travel_routes["router_5_area_1__TO__router_2"][0]
-    #     line = 1
-

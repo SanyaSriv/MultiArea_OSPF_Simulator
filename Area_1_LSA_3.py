df89e595aedb474aeaf59dfadb20dcbef45acd0f
SanyaSriv <ssrivas7@ucsc.edu>
2023-06-06 02:57:54 -0700

    LSA 3 is kind of working

--- Area_1_LSA_3.py
+++ Area_1_LSA_3.py
@@ -19,10 +19,11 @@ router_7_text = [0, 0, 0]
 router_8_text = [0, 0, 0]
 router_9_text = [0, 0, 0]
 router_10_text = [0, 0, 0]
-router_11_text = [0, 0, 0]
+router_11_text = [0, 0, 0] # this is ABR 2
 router_12_text = [0, 0, 0]
 router_13_text = [0, 0, 0]
 router_14_text = [0, 0, 0]
+router_ABR1_text = [0,0,0]
 
 increase_position = 10
 
@@ -39,6 +40,7 @@ def initialize_common_variables():
     global router_5_text, router_6_text
     global router_7_text, router_8_text, router_9_text, router_10_text
     global router_11_text, router_12_text, router_13_text, router_14_text
+    global router_ABR1_text
 
     router_1_text = [220, 250, 1]
     router_2_text = [405, 375, 1]
@@ -50,11 +52,11 @@ def initialize_common_variables():
     router_8_text = [460, 280, 1]
     router_9_text = [610, 460, 1]
     router_10_text = [750, 320, 1]
-    router_11_text = [1030, 110, 1]
-    router_12_text = [1030, 260, 1]
-    router_13_text = [1220, 110, 1]
-    router_14_text = [1220, 325, 1]
-
+    router_11_text = [950, 245, 1]
+    router_12_text = [1030, 385, 1]
+    router_13_text = [1220, 245, 1]
+    router_14_text = [1220, 455, 1]
+    router_ABR1_text = [350, 200, 1]
 
 def initialize_Area1_LSA():
     global list_text_position_area_1, text_dictionary_LSA_3_area_1
@@ -93,7 +95,7 @@ def initialize_Area1_LSA():
                                     area_3_travel_routes["router_14__TO__router_12"][0],
                                     area_3_travel_routes["router_14__TO__router_12"][1]])
     router_2_t2.update_starting_router("ABR_1")
-    router_2_t2.update_router_path_names(["router_7", "router_10", "router_11", "router_13", "router_14", "router_12"])
+    router_2_t2.update_router_path_names(["router_7", "router_10", "router_11: ABR_2", "router_13", "router_14", "router_12"])
 
     list_of_Area_1_triangles.append(router_2_t1)
     list_of_Area_1_triangles.append(router_2_t2)
@@ -116,8 +118,10 @@ def SendArea1LSA3(pygame, screen):
         if (to_be_removed == 1):
             list_of_Area_1_triangles.remove(i) # so we do not have a permanent triangle stationed on the last router
         if (router_reached != None):
-            text_string = "Recieved LSA 3: {}".format(router_started)
-            dictionary_text_string = "{} Recieved LSA 3 from {}".format(router_reached, router_started)
+            text_string = "Recieved LSA 3: Area 1"
+            dictionary_text_string = "{} Recieved LSA 3 from Area 1".format(router_reached)
+            # text_string = "Recieved LSA 3: {}".format(router_started)
+            # dictionary_text_string = "{} Recieved LSA 3 from {}".format(router_reached, router_started)
             
             if (dictionary_text_string in text_dictionary_LSA_3_area_1):
                 continue
@@ -136,7 +140,7 @@ def SendArea1LSA3(pygame, screen):
             elif router_reached == "router_10":
                 list_text_position_area_1.append([text_string, (router_10_text[0], router_10_text[1])])
                 router_10_text[1] += increase_position
-            elif router_reached == "router_11":
+            elif router_reached == "router_11: ABR_2":
                 list_text_position_area_1.append([text_string, (router_11_text[0], router_11_text[1])])
                 router_11_text[1] -= increase_position
             elif router_reached == "router_12":
@@ -148,6 +152,10 @@ def SendArea1LSA3(pygame, screen):
             elif router_reached == "router_14":
                 list_text_position_area_1.append([text_string, (router_14_text[0], router_14_text[1])])
                 router_14_text[1] += increase_position
+            elif router_reached == "ABR_1":
+                list_text_position_area_1.append([text_string, (router_ABR1_text[0], router_ABR1_text[1])])
+                router_ABR1_text[1] -= increase_position
+            
         
     for i in list_text_position_area_1:
         text = font.render(i[0], True, (0, 0, 0))
@@ -212,19 +220,17 @@ def initialize_Area2_LSA():
     router_7_t3.update_starting_router("ABR_1")
     router_7_t3.update_router_path_names(["router_2", "router_5", "router_4", "router_3"])
 
-    router_10_t1 = LSATriangles(inter_area_routes["router_10__TO__router_11"][0],
-                                inter_area_routes["router_10__TO__router_11"][1],
+    router_10_t1 = LSATriangles(area_3_travel_routes["router_11__TO__router_13"][0],
+                                area_3_travel_routes["router_11__TO__router_13"][1],
                                 (117, 227, 104))
-    router_10_t1.enable_full_route([inter_area_routes["router_10__TO__router_11"][0],
-                                    inter_area_routes["router_10__TO__router_11"][1],
-                                    area_3_travel_routes["router_11__TO__router_13"][0],
+    router_10_t1.enable_full_route([area_3_travel_routes["router_11__TO__router_13"][0],
                                     area_3_travel_routes["router_11__TO__router_13"][1],
                                     area_3_travel_routes["router_13__TO__router_14"][0],
                                     area_3_travel_routes["router_13__TO__router_14"][1],
                                     area_3_travel_routes["router_14__TO__router_12"][0],
                                     area_3_travel_routes["router_14__TO__router_12"][1]])
-    router_10_t1.update_starting_router("router_10")
-    router_10_t1.update_router_path_names(["router_11", "router_13", "router_14", "router_12"])
+    router_10_t1.update_starting_router("router_11: ABR_2")
+    router_10_t1.update_router_path_names(["router_13", "router_14", "router_12"])
 
     list_of_Area_2_triangles.append(router_7_t1)
     list_of_Area_2_triangles.append(router_7_t2)
@@ -247,8 +253,10 @@ def SendArea2LSA3(pygame, screen):
         if (to_be_removed == 1):
             list_of_Area_2_triangles.remove(i) # so we do not have a permanent triangle stattioned on the last router
         if (router_reached != None):
-            text_string = "Recieved LSA 3: {}".format(router_started)
-            dictionary_text_string = "{} Recieved LSA 3 from {}".format(router_reached, router_started)
+            text_string = "Recieved LSA 3: Area 2"
+            dictionary_text_string = "{} Recieved LSA 3 from Area 2".format(router_reached)
+            # text_string = "Recieved LSA 3: {}".format(router_started)
+            # dictionary_text_string = "{} Recieved LSA 3 from {}".format(router_reached, router_started)
             
             if (dictionary_text_string in text_dictionary_LSA_3_area_2):
                 continue
@@ -274,7 +282,7 @@ def SendArea2LSA3(pygame, screen):
             elif router_reached == "router_6":
                 list_text_position_area_2.append([text_string, (router_6_text[0], router_6_text[1])])
                 router_6_text[router_6_text[2]] += increase_position
-            elif router_reached == "router_11":
+            elif router_reached == "router_11: ABR_2":
                 list_text_position_area_2.append([text_string, (router_11_text[0], router_11_text[1])])
                 router_11_text[1] -= increase_position
             elif router_reached == "router_12":
@@ -286,6 +294,9 @@ def SendArea2LSA3(pygame, screen):
             elif router_reached == "router_14":
                 list_text_position_area_2.append([text_string, (router_14_text[0], router_14_text[1])])
                 router_14_text[1] += increase_position
+            elif router_reached == "ABR_1":
+                list_text_position_area_1.append([text_string, (router_ABR1_text[0], router_ABR1_text[1])])
+                router_ABR1_text[1] -= increase_position
 
     for i in list_text_position_area_2:
         text = font.render(i[0], True, (0, 0, 0))
@@ -321,7 +332,7 @@ def initialize_Area3_LSA():
                                     area_2_travel_routes["router_7__TO__router_10"][0],
                                     area_2_travel_routes["router_7__TO__router_8"][0],
                                     area_2_travel_routes["router_7__TO__router_8"][1]])
-    router_11_t1.update_starting_router("router_11")
+    router_11_t1.update_starting_router("router_11: ABR_2")
     router_11_t1.update_router_path_names(["router_10", "router_7", "router_8"])
 
     router_11_t2 = LSATriangles(inter_area_routes["router_10__TO__router_11"][1],
@@ -333,7 +344,7 @@ def initialize_Area3_LSA():
                                     area_2_travel_routes["router_9__TO__router_10"][0],
                                     area_2_travel_routes["router_8__TO__router_9"][1],
                                     area_2_travel_routes["router_8__TO__router_9"][0]])
-    router_11_t2.update_starting_router("router_11")
+    router_11_t2.update_starting_router("router_11: ABR_2")
     router_11_t2.update_router_path_names(["router_10", "router_9", "router_8"])
     
     router_11_t3 = LSATriangles(inter_area_routes["router_10__TO__router_11"][1],
@@ -349,7 +360,7 @@ def initialize_Area3_LSA():
                                     inter_area_routes["router_2__TO__router_abr1"][1], 
                                     area_1_travel_routes["router_2__TO__router_6"][0],
                                     area_1_travel_routes["router_2__TO__router_6"][1]])
-    router_11_t3.update_starting_router("router_11")
+    router_11_t3.update_starting_router("router_11: ABR_2")
     router_11_t3.update_router_path_names(["router_10", "router_7", "ABR_1", "router_2", "router_6"])
     
     router_11_t4 = LSATriangles(inter_area_routes["router_10__TO__router_11"][1],
@@ -368,7 +379,7 @@ def initialize_Area3_LSA():
                                     area_1_travel_routes["router_5_area_1__TO__router_1"][1],
                                     area_1_travel_routes["router_5_area_1__TO__router_1"][0]])
     
-    router_11_t4.update_starting_router("router_11")
+    router_11_t4.update_starting_router("router_11: ABR_2")
     router_11_t4.update_router_path_names(["router_10", "router_7", "ABR_1", "router_2", "router_5", "router_1"])
 
     router_11_t5 = LSATriangles(inter_area_routes["router_10__TO__router_11"][1],
@@ -389,7 +400,7 @@ def initialize_Area3_LSA():
                                     area_1_travel_routes["router_4__TO__router_3"][1],
                                     area_1_travel_routes["router_4__TO__router_3"][0]])
     
-    router_11_t5.update_starting_router("router_11")
+    router_11_t5.update_starting_router("router_11: ABR_2")
     router_11_t5.update_router_path_names(["router_10", "router_7", "ABR_1", "router_2", "router_5", "router_4", "router_3"])
 
     list_of_Area_3_triangles.append(router_11_t1)
@@ -416,8 +427,8 @@ def SendArea3LSA3(pygame, screen):
         if (to_be_removed == 1):
             list_of_Area_3_triangles.remove(i) # so we do not have a permanent triangle stattioned on the last router
         if (router_reached != None):
-            text_string = "Recieved LSA 3: {}".format(router_started)
-            dictionary_text_string = "{} Recieved LSA 3 from {}".format(router_reached, router_started)
+            text_string = "Recieved LSA 3: Area 3"
+            dictionary_text_string = "{} Recieved LSA 3 from Area 3".format(router_reached)
             
             if (dictionary_text_string in text_dictionary_LSA_3_area_3):
                 continue
@@ -454,6 +465,9 @@ def SendArea3LSA3(pygame, screen):
             elif router_reached == "router_10":
                 list_text_position_area_3.append([text_string, (router_10_text[0], router_10_text[1])])
                 router_10_text[1] += increase_position
+            elif router_reached == "ABR_1":
+                list_text_position_area_1.append([text_string, (router_ABR1_text[0], router_ABR1_text[1])])
+                router_ABR1_text[1] -= increase_position
 
     for i in list_text_position_area_3:
         text = font.render(i[0], True, (0, 0, 0))
